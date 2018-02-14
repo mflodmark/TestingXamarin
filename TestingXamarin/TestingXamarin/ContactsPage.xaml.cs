656803ddc53a793c06b1372068a09e2f47db6b7d
Markus Flodmark <markus.flodmark@gmail.com>
2018-02-14 06:10:12 +0100

    contacts

---
+++ TestingXamarin/TestingXamarin/ContactsPage.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TestingXamarin.Models;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace TestingXamarin
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class ContactsPage : ContentPage
+	{
+		public ContactsPage ()
+		{
+			InitializeComponent ();
+
+		    listView.ItemsSource = new List<Contact> {
+		        new Contact { Name = "Mosh", ImageUrl = "http://lorempixel.com/100/100/people/1" },
+		        new Contact { Name = "John", ImageUrl = "http://lorempixel.com/100/100/people/2", Status = "Hey, let's talk!" }
+		    };
+        }
+	}
+}
